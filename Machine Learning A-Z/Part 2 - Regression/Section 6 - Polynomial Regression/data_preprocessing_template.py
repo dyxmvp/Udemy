abe9b6fb0aae7d2177097f69b54d356c3121b2f8
D YX <D YX>
2017-06-23 07:53:42 -0400

    SVM template

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
@@ -42,8 +42,16 @@ plt.xlabel('Salary')
 plt.show()
 
 # Visualising the Polynomial Regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'green')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'green')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Salary')
 plt.show()
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result with Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
